3571f29d6750dfe8a9d4fa1d18720f1ae1622ce3
Hans Fangohr <hans.fangohr@mpsd.mpg.de>
2022-10-07 07:21:04 +0200

    exit script on error

--- build-and-test-most-recent-versions.sh
+++ build-and-test-most-recent-versions.sh
@@ -1,19 +1,29 @@
 #!/bin/bash
 
+# exit script on error
+set -e
+
 # check the last 4 versions 
 VERSIONS=`spack info oommf | grep -A 4 "Safe versions:" | awk 'NR>1' | awk '{print $1}'`
 
 echo $VERSIONS
 for VER in $VERSIONS; do
     date
-    echo "Working on $VER ...";
+    spack install does-not-exist
+    echo "Installing $VER ...";
     spack install oommf@$VER
-
+    
     echo "completed installation $VER"
     echo "============================================"
     
 done
+
+# run smoke tests for all versions of oommf
 spack test run --alias testname oommf
+
+# display results
 spack test results -l testname | tee spack-test-results-output.txt
+
+# summarise what versions have been installed
 spack find -v -l oommf | tee spack-find-oommf-output.txt
 
