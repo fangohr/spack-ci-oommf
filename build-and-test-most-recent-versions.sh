a1dbab533213ef67fe9dbdef79b90a138299b3d9
Hans Fangohr <hans.fangohr@mpsd.mpg.de>
2022-10-07 07:13:58 +0200

    Test the 4 latest oommf versions in spack

--- build-and-test-most-recent-versions.sh
+++ build-and-test-most-recent-versions.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+
+# check the last 4 versions 
 VERSIONS=`spack info oommf | grep -A 4 "Safe versions:" | awk 'NR>1' | awk '{print $1}'`
 
 echo $VERSIONS
@@ -7,18 +9,11 @@ for VER in $VERSIONS; do
     echo "Working on $VER ...";
     spack install oommf@$VER
 
-    # run spack smoke tests for oommf. We get an error if any of the fail.
-    RUN . $SPACK_ROOT/share/spack/setup-env.sh && spack test run --alias testname$VER oommf@$VER
-    # display output from smoke tests (just for information)
-    RUN . $SPACK_ROOT/share/spack/setup-env.sh && spack test results -l testname$VER
-
-    # show oommf version at very end of output 
-    RUN . $SPACK_ROOT/share/spack/setup-env.sh && spack load oommf@$VER && oommf.tcl +version
-
-    # uninstall OOMMF (avoid conflicts with multiple versions installed)
-    RUN . $SPACK_ROOT/share/spack/setup-env.sh && spack unload oommf@$VER && spack uninstall -y oommf@VER
-
-    echo "completed $VER"
+    echo "completed installation $VER"
     echo "============================================"
     
 done
+spack test run --alias testname oommf
+spack test results -l testname | tee spack-test-results-output.txt
+spack find -v -l oommf | tee spack-find-oommf-output.txt
+
